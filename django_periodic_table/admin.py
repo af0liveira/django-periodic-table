9fc485e9d918fc4b7e87bcd57390740ee2c422cc
af0liveira <af0liveira@icloud.com>
2023-09-06 23:34:26 +0200

    Add model admin for the elements database.
    Element data can be viewed and manipulated on the Django Admin page.
    Note that some attributes are _not_ part of the databae, but properties
    of the Element model, obtained from getters using the periodictable
    module.
    Ideally, the properties would be part of the  database, no extract from
    external sources.

--- django_periodic_table/admin.py
+++ django_periodic_table/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Element
 
+class ElementTable(admin.ModelAdmin):
+    """Representation of the Elements table."""
+    list_display = (
+        'id',
+        'atomic_number',
+        'atomic_mass',
+        'symbol',
+        'group',
+    )
+    list_per_page = 25
+    ordering = ['atomic_number', 'id']
+
+    def get_atomic_mass(self, obj):
+        return obj.atomic_mass
+    
+
+admin.site.register(Element, ElementTable)
 # Register your models here.
